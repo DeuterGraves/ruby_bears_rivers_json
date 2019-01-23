afb417127e2e2f2aacb0e530802493ffc1577484
DeuterGraves <carolinetgraves@gmail.com>
2019-01-23 13:17:07 +0000

    data now coming through

--- models/rivers.rb
+++ models/rivers.rb
@@ -1,14 +1,14 @@
 require('json')
 
 class Rivers
-    attr_accessor
+    attr_accessor :fish_data
 
     def initialize()
         @fish_data = []
     end
 
     def getRiverData()
-        data = File.read('../data/fishes.json')
+        data = File.read('./data/fishes.json')
         river_stats = JSON.parse(data)
         @fish_data << river_stats
     end
