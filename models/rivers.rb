5c227d5376a605e3e0bab116f281120dc92553b4
DeuterGraves <carolinetgraves@gmail.com>
2019-01-23 15:38:12 +0000

    checking if the river is in the keys, printing message if the river doesn't exist

--- models/rivers.rb
+++ models/rivers.rb
@@ -1,4 +1,5 @@
 require('json')
+require('pry')
 
 class Rivers
     attr_accessor :fish_data
@@ -9,11 +10,34 @@ class Rivers
     end
 
     def getFishValue(river, fish)
+        # binding.pry
         fish_value = @fish_data[river][fish]
         return fish_value.to_f
     end
 
+    def isRiver?(river)
+        #check the keys
+        rivers = @fish_data.keys
+        # binding.pry
+        for riverName in rivers
+            if  river == riverName
+                return true
+            end
+        end
+        return false
+    end
 
+    def getFishIfRiver(river,fish)
+        if isRiver?(river)
+            getFishValue(river, fish)
+        else
+            # raise "I'm sorry Dave, that's not a river"
+            p "I'm sorry Dave, that's not a river"
+        end
+    end
+
+
+#class end
 end
 
 
--- spec/bear_spec.rb
+++ spec/bear_spec.rb
@@ -8,8 +8,6 @@ class BearTest < MiniTest::Test
     def setup()
         @bear = Bear.new("Tony", 15, 0)
         @rivers = Rivers.new()
-
-        # @rivers.getRiverData()
     end
 
     def test_bear_has_name()
@@ -23,8 +21,28 @@ class BearTest < MiniTest::Test
 
     def test_river_gets_fish_value_5()
         fish_value = 5
-        binding.pry
+        # binding.pry
         assert_equal(fish_value, @rivers.getFishValue("Potomac", "Susan"))
+    end
+
+    def test_river_doesnt_exist()
+        assert_equal(false, @rivers.isRiver?("Nile"))
+    end
+
+    def test_river_does_exist()
+        assert_equal(true, @rivers.isRiver?("Plata"))
+    end
+
+    def test_no_river_exception()
+        assert_equal("I'm sorry Dave, that's not a river", @rivers.getFishIfRiver("Amazon", "Bob"))
+    end
+
+    def test_gets_fish_if_river()
+        assert_equal(4, @rivers.getFishIfRiver("Mississippi", "Drew"))
+    end
+
+    def test_isRiver?()
+        assert_equal(true, @rivers.isRiver?("Mississippi"))
     end
 
 end
